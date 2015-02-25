2cc2494cc3b73fc1e583987bcf93c02e6935efa2
marce <marce@marce.com>
2015-02-25 18:34:29 +0100

    creacion class MiHashMap que simula el coportamiento de una clase  HashMap con claves String y valores int

---
+++ MiHashMap.java
@@ -0,0 +1,21 @@
+/**
+ * clase que imita el comportamiento de la clase HashMap con claves tipo String y valores int
+ * 
+ * @author Marcelino
+ * @version beta
+ */
+public class MiHashMap
+{
+    // instance variables - replace the example below with your own
+    private String[] key;
+    private int[] value;
+
+    /**
+     * Constructor for objects of class MiHashMap
+     */
+    public MiHashMap()
+    {
+        key   = new String[0];
+        value = new int[0];
+    }
+}
